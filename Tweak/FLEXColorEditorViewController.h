d316aa4630b28f925add3a38c0cf6b053c39529a
simongellesch4726-hash <simongellesch4726@gmail.com>
2026-09-06 22:33:47 +0200

    Add FLEX color editor interface

---
+++ Tweak/FLEXColorEditorViewController.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FLEXColorEditorViewController : UITableViewController
+- (instancetype)initWithView:(UIView *)view;
+@end
+
+NS_ASSUME_NONNULL_END
