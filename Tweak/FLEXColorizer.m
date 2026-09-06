95e1b295e4804dbb1f87abe8b3a055eacbc0a107
simongellesch4726-hash <simongellesch4726@gmail.com>
2026-09-06 22:35:17 +0200

    Wire FLEX color action into explorer safely

--- Tweak/FLEXColorizer.m
+++ Tweak/FLEXColorizer.m
@@ -1,30 +1,70 @@
 #import <UIKit/UIKit.h>
-#import "../Classes/FLEXManager.h"
+#import "../Classes/Manager/FLEXManager.h"
 #import "../Classes/ExplorerInterface/FLEXExplorerViewController.h"
 #import "../Classes/Toolbar/FLEXExplorerToolbar.h"
 #import "../Classes/Toolbar/FLEXExplorerToolbarItem.h"
 #import "FLEXColorEditorViewController.h"
 #import <objc/runtime.h>
 
-static FLEXExplorerViewController *FLEXCurrentExplorer(void) {
-    FLEXManager *manager = [FLEXManager sharedManager];
-    id explorer = [manager valueForKey:@"explorerViewController"];
-    return [explorer isKindOfClass:[FLEXExplorerViewController class]] ? explorer : nil;
+static const void *FLEXColorItemKey = &FLEXColorItemKey;
+
+@interface FLEXExplorerToolbar (FLEXColorizerPrivate)
+@property (nonatomic, readonly) FLEXExplorerToolbarItem *flex_colorItem;
+- (void)flex_setColorItem:(FLEXExplorerToolbarItem *)item;
+@end
+
+@implementation FLEXExplorerToolbar (FLEXColorizerPrivate)
+- (FLEXExplorerToolbarItem *)flex_colorItem {
+    return objc_getAssociatedObject(self, FLEXColorItemKey);
+}
+- (void)flex_setColorItem:(FLEXExplorerToolbarItem *)item {
+    objc_setAssociatedObject(self, FLEXColorItemKey, item, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+@end
+
+@implementation FLEXExplorerViewController (FLEXColorizer)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Method original = class_getInstanceMethod(self, @selector(viewDidLoad));
+        Method replacement = class_getInstanceMethod(self, @selector(flex_viewDidLoad_colorizer));
+        if (original && replacement) method_exchangeImplementations(original, replacement);
+
+        original = class_getInstanceMethod(self, @selector(updateButtonStates));
+        replacement = class_getInstanceMethod(self, @selector(flex_updateButtonStates_colorizer));
+        if (original && replacement) method_exchangeImplementations(original, replacement);
+    });
 }
 
-static void FLEXInstallColorAction(FLEXExplorerViewController *explorer) {
-    if (!explorer) return;
-    FLEXExplorerToolbar *toolbar = explorer.explorerToolbar;
-    if (![toolbar respondsToSelector:@selector(colorItem)]) return;
+- (void)flex_viewDidLoad_colorizer {
+    [self flex_viewDidLoad_colorizer];
+
+    FLEXExplorerToolbar *toolbar = self.explorerToolbar;
+    if (toolbar.flex_colorItem) return;
+
+    UIImage *image = nil;
+    if (@available(iOS 13.0, *)) image = [UIImage systemImageNamed:@"paintpalette.fill"];
+    if (!image) image = [UIImage systemImageNamed:@"paintbrush.fill"];
+
+    FLEXExplorerToolbarItem *item = [FLEXExplorerToolbarItem itemWithTitle:@"color" image:image];
+    item.enabled = NO;
+    [item addTarget:self action:@selector(flex_colorButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [toolbar flex_setColorItem:item];
 }
 
-__attribute__((constructor)) static void FLEXColorizerInit(void) {
-    @autoreleasepool {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            Class toolbarClass = [FLEXExplorerToolbar class];
-            Class explorerClass = [FLEXExplorerViewController class];
-            (void)toolbarClass;
-            (void)explorerClass;
-        });
-    }
+- (void)flex_updateButtonStates_colorizer {
+    [self flex_updateButtonStates_colorizer];
+    FLEXExplorerToolbar *toolbar = self.explorerToolbar;
+    FLEXExplorerToolbarItem *item = toolbar.flex_colorItem;
+    if (item) item.enabled = ([self valueForKey:@"selectedView"] != nil);
 }
+
+- (void)flex_colorButtonTapped:(FLEXExplorerToolbarItem *)sender {
+    UIView *view = [self valueForKey:@"selectedView"];
+    if (!view) return;
+    FLEXColorEditorViewController *editor = [[FLEXColorEditorViewController alloc] initWithView:view];
+    [[FLEXManager sharedManager] presentEmbeddedTool:editor completion:nil];
+}
+
+@end
